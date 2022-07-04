426abddc00be3f8f2eeb485df31315ecdd38c4f5
vietkynl99 <107388318+vietkynl99@users.noreply.github.com>
2022-07-04 11:00:02 +0700

    Edit input message and Makefile

---
+++ program/msg_server.c
@@ -0,0 +1,9 @@
+#include "server.h"
+#include "client.h"
+#include "common.h"
+
+int main()
+{
+    printf("hello \n");
+    return 0;
+}
--- program/server.h
+++ program/server.h
@@ -1,8 +1,29 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <errno.h> 
+#include <signal.h>
+#include <unistd.h>  
+#include <termios.h>
+#include <pthread.h>
+#include <sys/wait.h>
 
 
+#define EXIT_STRING             ":q"
+#define INPUT_BUFFER_SIZE       255
 
+
+char getch(void); 
+void printMessage(int, char *);                 
+void showBuffer();                              
+void insertNewMessage(char *);                  
+int checkValidInputMessage(char *);      
+void processInput(char, void (*)());   
 
 #endif
