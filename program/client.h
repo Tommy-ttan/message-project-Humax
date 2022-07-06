c360e7f5c0b860a1f2c97f146fb48204db42af60
U-CONIN\ttan <ttan@Hvn_Antt.humaxdigital.com>
2022-07-06 10:26:25 +0700

    rewrite struct client

--- program/client.h
+++ program/client.h
@@ -1,7 +1,15 @@
 #ifndef __CLIENT_H__
 #define __CLIENT_H__
 
-
+struct client 
+{
+    int fd;
+    int role;
+    char *send_buff[];
+    char *recv_buff[];
+    void (*pfunc_send)();
+    void (*pfunc_recv)();
+}
 
 
 
