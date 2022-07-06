d70a7aba652a76d71823b350bcef922db3b63127
U-CONIN\ttan <ttan@Hvn_Antt.humaxdigital.com>
2022-07-06 13:32:37 +0700

    add comment

--- program/common.c
+++ program/common.c
@@ -1,10 +1,24 @@
 #include "common.h"
 
+/**
+ * @brief send mess (from buffer)
+ * 
+ * @param fd
+ * @param buff 
+ * @return void
+ */
 void sendMess(int fd, char buff[])
 {
     write(fd, buff, BUFFER_SIZE);
 }
 
+/**
+ * @brief receive message
+ * 
+ * @param fd
+ * @param buff 
+ * @return void
+ */
 void recvMess(int fd, char buff[])
 {
     read(fd, buff, BUFFER_SIZE);
