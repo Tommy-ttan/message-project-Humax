6df23248a95605cedc79b5372d9a297436032b75
nvmanh1 <nguyenmanh.cn.1998@gmail.com>
2022-07-06 11:08:25 +0700

    add initClent() connectServer() chooseRole() funcrtion

--- program/client.h
+++ program/client.h
@@ -1,6 +1,19 @@
 #ifndef __CLIENT_H__
 #define __CLIENT_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <netinet/in.h> 
+#include <netdb.h> 
+
+#define SOCKET_PORT	    5000
+
+int initClient(void);
+int connectServer(void);
+int chooseRole(void);
+
 
 
 
